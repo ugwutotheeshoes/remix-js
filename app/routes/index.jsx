469d5b209438b3dad5a43be446b504d90eadbfa3
Emmanuel Ugwu <>
2023-12-11 21:28:44 +0100

    Initial commit

---
+++ app/routes/index.jsx
@@ -0,0 +1,14 @@
+import "../style/index.css";
+import React, { useState } from "react";
+import Questions from "./Questions";
+import { Client, Databases } from "appwrite";
+
+export default function Index() {
+  
+
+  return (
+    <div>
+      <h1>Remix</h1>
+    </div>
+  );
+}
